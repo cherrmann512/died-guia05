1502d1039e6178d98789da53873ece1cff49b562
cherrmann512 <cherrmann512@gmail.com>
2020-04-06 23:04:35 -0300

    problema 1 resuelto y problema 2 casi

--- src/died/guia5/problema2/Pedido.java
+++ src/died/guia5/problema2/Pedido.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public abstract class Pedido {
 	
+	LocalDate fechaEntrega;
 	public abstract boolean agregarProducto (Producto p);
 	public abstract double precio();
 	public abstract double comision();
